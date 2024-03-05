d216413fb70a5e7e2401dc6e0a00807c2e1e2999
Marcella Rappoli Bedinelli <105222261+marappoli@users.noreply.github.com>
2024-03-05 16:30:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from chatbot import ChatBot
+myChatBot = ChatBot()
+#apenas carregar um modelo pronto
+
+myChatBot.createModel()
+
+#myChatBot.loadModel()
+
+
+print("Bem vindo ao Chatbot")
+
+
+pergunta = input("como posso te ajudar?")
+resposta, intencao = myChatBot.chatbot_response(pergunta)
+print(resposta + "   ["+intencao[0]['intent']+"]")
+
+
+while (intencao[0]['intent']!="despedida"):
+    pergunta = input("posso lhe ajudar com algo a mais?")
+    resposta, intencao = myChatBot.chatbot_response(pergunta)
+    print(resposta + "   [" + intencao[0]['intent'] + "]")
+
+print("Foi um prazer atendê-lo")
